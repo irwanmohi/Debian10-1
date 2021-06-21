fe55ac08cd04b0b65ba02aabf43ca2498c7e6d81
cybertize <43793878+cybertize-dev@users.noreply.github.com>
2021-06-22 04:44:30 +0800

    Update openvpn.bash

--- scripts/openvpn.bash
+++ scripts/openvpn.bash
@@ -31,7 +31,7 @@ cd /usr/share/easy-rsa
 cp -R /usr/share/easy-rsa/pki /etc/openvpn/
 echo " Done"
 
-# /etc/openvpn/servcustom.conf
+# /etc/openvpn/server.conf
 {
   echo "# OVPN SERVER-CUSTOM CONFIG
 # ----------------------------
@@ -62,77 +62,8 @@ log ovpn.log
 verb 3
 mute 10
 plugin /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so login
-username-as-common-name" > /etc/openvpn/servcustom.conf
+username-as-common-name" > /etc/openvpn/server.conf
 } &>/dev/null
-
-# /etc/openvpn/servstunnel.conf
-{
-  echo "# OVPN SERVER-STUNNEL CONFIG
-# ----------------------------
-port 6545
-proto tcp
-dev tun
-
-ca /etc/openvpn/pki/ca.crt
-cert /etc/openvpn/pki/issued/server.crt
-key /etc/openvpn/pki/private/server.key
-dh /etc/openvpn/pki/dh.pem
-tls-auth /etc/openvpn/pki/ta.key 0
-
-verify-client-cert none
-server 10.9.0.0 255.255.255.0
-ifconfig-pool-persist ipp.txt
-push \"redirect-gateway def1 bypass-dhcp\"
-push \"dhcp-option DNS 1.1.1.1\"
-push \"dhcp-option DNS 1.0.0.1\"
-keepalive 10 120
-cipher AES-256-CBC
-user nobody
-group nogroup
-persist-key
-persist-tun
-status status.log
-log ovpn.log
-verb 3
-mute 10
-plugin /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so login
-username-as-common-name" > /etc/openvpn/servstunnel.conf
-} &>/dev/null
-
-# /etc/openvpn/servobfs.conf
-{
-  echo "# OVPN SERVER-OBFS CONFIG
-# ----------------------------
-port 6753
-proto tcp
-dev tun
-
-ca /etc/openvpn/pki/ca.crt
-cert /etc/openvpn/pki/issued/server.crt
-key /etc/openvpn/pki/private/server.key
-dh /etc/openvpn/pki/dh.pem
-tls-auth /etc/openvpn/pki/ta.key 0
-
-verify-client-cert none
-server 10.10.0.0 255.255.255.0
-ifconfig-pool-persist ipp.txt
-push \"redirect-gateway def1 bypass-dhcp\"
-push \"dhcp-option DNS 8.8.8.8\"
-push \"dhcp-option DNS 8.8.4.4\"
-keepalive 10 120
-cipher AES-256-CBC
-user nobody
-group nogroup
-persist-key
-persist-tun
-status status.log
-log ovpn.log
-verb 3
-mute 10
-plugin /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so login
-username-as-common-name" > /etc/openvpn/servobfs.conf
-} &>/dev/null
-
 
 # customClient.conf - Custom client config file
 {
@@ -194,34 +125,6 @@ cat /etc/openvpn/pki/ta.key >> /etc/openvpn/client/client-stunnel.conf
 echo "</tls-auth>" >> /etc/openvpn/client/client-stunnel.conf
 } &>/dev/null
 
-# obfs4Client.conf - obfsproxy client config file
-{
-  echo "# OVPN CLIENT-OBFS4 CONFIG
-# ----------------------------
-client
-pull
-dev tun
-proto tcp
-remote $ipaddr 6753
-socks-proxy 127.0.0.1 9090
-route $ipaddr 255.255.255.255 net_gateway
-resolv-retry infinite
-persist-key
-persist-tun
-script-security 3
-auth-user-pass
-verb 3" > /etc/openvpn/client/client-obfs.conf
-
-echo "" >> /etc/openvpn/client/client-obfs.conf
-echo "<ca>" >> /etc/openvpn/client/client-obfs.conf
-cat /etc/openvpn/pki/ca.crt >> /etc/openvpn/client/client-obfs.conf
-echo "</ca>" >> /etc/openvpn/client/client-obfs.conf
-echo "" >> /etc/openvpn/client/client-obfs.conf
-echo "<tls-auth>" >> /etc/openvpn/client/client-obfs.conf
-cat /etc/openvpn/pki/ta.key >> /etc/openvpn/client/client-obfs.conf
-echo "</tls-auth>" >> /etc/openvpn/client/client-obfs.conf
-} &>/dev/null
-
 echo ""
 echo -e "${GREEN}Congratulation, we are done with openvpn setup${PLAIN}"
 echo ""
@@ -236,7 +139,5 @@ echo -e "${YELLOW}Stunnel Client:${PLAIN} ${GREEN}6545${PLAIN}"
 echo -e "${YELLOW}Obfs Client:${PLAIN} ${GREEN}6753${PLAIN}"
 echo "----------------------------------------------"
 echo -e "${YELLOW}Start OpenVPN services:${PLAIN}"
-echo -e "${GREEN}systemctl start openvpn@servcustom${PLAIN}"
-echo -e "${GREEN}systemctl start openvpn@servstunnel${PLAIN}"
-echo -e "${GREEN}systemctl start openvpn@servobfs${PLAIN}"
+echo -e "${GREEN}systemctl start openvpn@server${PLAIN}"
 echo "=============================================="
