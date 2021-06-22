85c984b6b48456f03c85d506ad987ff91da71540
cybertize <43793878+cybertize-dev@users.noreply.github.com>
2021-06-22 14:03:35 +0800

    Update badvpn.bash

--- scripts/badvpn.bash
+++ scripts/badvpn.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+RED="\033[31;1m"
+GREEN="\033[32;1m"
+YELLOW="\033[33;1m"
+BLUE="\033[34;1m"
+PURPLE="\033[35;1m"
+CYAN="\033[36;1m"
+PLAIN="\033[0m"
+
 ipaddr=$( ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | head -1 )
-. root/Debian10-main/_sources/includes/colors
 
 until [[ $YESNO =~ (y|n) ]]; do
   read -rp "Do you want to continue? [y/n]: " YESNO
